16be0d0be5fb9f5a0ca125ce357ba393d77ae90d
mohamed <mohamed@gmail.com>
2024-05-15 14:27:54 +0200

    :tada: first commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+
+public class Calculator {
+
+    public int addition(String arguments) {
+
+        int sum = 0;
+        for (String add : arguments.split("\\+"))
+            sum += Integer.valueOf(add);
+            return sum;
+    }
+}
